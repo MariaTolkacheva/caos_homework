6b32cab2ff049306dc7783970d15cb42f5394353
Maria Tolkacheva <mvtolkacheva@edu.hse.ru>
2025-06-19 18:59:55 +0300

    why?

--- os/lab05/01_regex_replace/solution.c
+++ os/lab05/01_regex_replace/solution.c
@@ -6,16 +6,24 @@
 enum { INITIAL_BUFFER_SIZE = 2048, SP = 2 };
 
 int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    fprintf(stderr, "wrong number of args");
+    return EXIT_FAILURE;
+  }
 
   const char *pattern = argv[1];
   const char *text = argv[2];
   const char *replacement = argv[3];
 
   regex_t regex;
+  if (regcomp(&regex, pattern, REG_EXTENDED)) {
+    return EXIT_FAILURE;
+  }
+
   size_t buffer_size = INITIAL_BUFFER_SIZE;
   char *result = (char *)malloc(buffer_size);
   if (!result) {
-    perror("malloc failed (unlucky)");
+    perror("malloc failed");
     regfree(&regex);
     return EXIT_FAILURE;
   }
@@ -23,39 +31,48 @@ int main(int argc, char *argv[]) {
   result[0] = '\0';
   size_t result_len = 0;
   regmatch_t match;
-
   const char *p = text;
 
   while (regexec(&regex, p, 1, &match, 0) == 0) {
     size_t prefix_len = match.rm_so;
+    size_t replacement_len = strlen(replacement);
+    size_t needed_size = result_len + prefix_len + replacement_len + 1;
 
-    while (result_len + prefix_len + strlen(replacement) + 1 >= buffer_size) {
-      buffer_size *= SP;
-      result = (char *)realloc(result, buffer_size);
-      if (!result) {
-        perror("lloc failed (unlucky)");
+    if (needed_size >= buffer_size) {
+      while (buffer_size <= needed_size) {
+        buffer_size *= SP;
+      }
+      char *new_result = (char *)realloc(result, buffer_size);
+      if (!new_result) {
+        perror("realloc failed");
+        free(result);
         regfree(&regex);
         return EXIT_FAILURE;
       }
+      result = new_result;
     }
 
-    strncat(result + result_len, p, prefix_len);
+    strncpy(result + result_len, p, prefix_len);
     result_len += prefix_len;
-    strcat(result + result_len, replacement);
-    result_len += strlen(replacement);
+    strcpy(result + result_len, replacement);
+    result_len += replacement_len;
     p += match.rm_eo;
   }
 
-  //(МАША) добавь обработки
   size_t remaining = strlen(p);
-  while (result_len + remaining + 1 >= buffer_size) {
-    buffer_size *= SP;
-    result = (char *)realloc(result, buffer_size);
-    if (!result) {
-      perror("lloc failed (unlucky)");
+  size_t needed_size = result_len + remaining + 1;
+  if (needed_size >= buffer_size) {
+    while (buffer_size <= needed_size) {
+      buffer_size *= SP;
+    }
+    char *new_result = (char *)realloc(result, buffer_size);
+    if (!new_result) {
+      perror("realloc failed (unlucky)");
+      free(result);
       regfree(&regex);
       return EXIT_FAILURE;
     }
+    result = new_result;
   }
   strcat(result + result_len, p);
   printf("%s\n", result);
